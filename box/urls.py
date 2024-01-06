1e2c26dd45cfbe4948dc98b74088e4a1e36150cb
Zaidh <zadykhan29@gmail.com>
2024-01-06 20:04:28 +0000

    7th commit
    redirecting user after login

--- box/urls.py
+++ box/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
-from .views import HomePage,update_profile
+from .views import HomePage,update_profile,CustomLoginView
 
 app_name = "box"
 
 urlpatterns = [
     path('', HomePage, name='home'),
     path("accounts/profile/", update_profile, name='profile'),
+    path("accounts/login/", CustomLoginView.as_view(), name='account_login'),
     path("events/", views.events_list, name='events_list'),
     path("events/<int:event_id>/register/", views.register_event, name='register_event'),
 ]
--- box/views.py
+++ box/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db import transaction
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.urls import reverse
+from allauth.account.views import LoginView
 from .forms import UserForm, UserProfileForm
 from .models import UserProfile, BoxingEvent
 
@@ -25,6 +28,14 @@ def update_profile(request):
         profile_form = UserProfileForm(instance=user_profile)
 
     return render(request, 'box/profile.html', {'user_form': user_form, 'profile_form': profile_form})
+
+class CustomLoginView(LoginView):
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        if self.user and not self.user.last_login:
+            messages.success(self.request, "Welcome! You have successfully logged in.")
+            return redirect(reverse('user:events_list'))
+        return response
 
 @login_required
 def events_list(request):
