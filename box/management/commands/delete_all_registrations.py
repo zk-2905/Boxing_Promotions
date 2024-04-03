c436065aaa8dd4ca7a39c2fe7ddfe1debf6ee75e
Zaidh <zadykhan29@gmail.com>
2024-04-03 14:30:20 +0100

    Update delete_all_registrations.py

--- box/management/commands/delete_all_registrations.py
+++ box/management/commands/delete_all_registrations.py
@@ -2,10 +2,10 @@ from django.core.management.base import BaseCommand
 from box.models import EventRegistration, Fight
 
 class Command(BaseCommand):
-    help = 'Deletes all events from the database'
+    help = 'Deletes all registrations from the database'
 
     def handle(self, *args, **options):
-        # Delete all events
+        # Delete all registrations
         EventRegistration.objects.all().delete()
         Fight.objects.all().delete()
         self.stdout.write(self.style.SUCCESS('All registrations have been deleted.'))
